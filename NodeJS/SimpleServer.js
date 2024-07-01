72d9fe172cd71b22b26a1642edfd2772a9296f28
Krupakar Reddy <137398727+Krupakar-Reddy-S@users.noreply.github.com>
2024-07-01 11:40:45 +0530

    Served from the index.html file

--- NodeJS/SimpleServer.js
+++ NodeJS/SimpleServer.js
@@ -1,11 +1,10 @@
 const http = require('http');
+const fs = require('fs');
 
 const server = http.createServer((req, res) => {
     res.setHeader('Content-Type', 'text/html');
 
-    res.write('<html> <head> <title>Node JS Class</title> </head> <body>');
-    res.write('<h1>Hello World!</h1>');
-    res.write('</body> </html>');
+    res.write(fs.readFileSync('./index.html'));
 
     res.end();
 });
