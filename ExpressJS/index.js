e14d3ca4423f097e0f51850202693aaa84ac4602
Krupakar Reddy <137398727+Krupakar-Reddy-S@users.noreply.github.com>
2024-07-03 10:25:51 +0530

    added POST, PUT methods code

--- ExpressJS/index.js
+++ ExpressJS/index.js
@@ -2,15 +2,44 @@ const express = require('express');
 const app = express();
 
 const port = 3000;
+app.use(express.json());
 
 let courses = [
-    { id: 1, name: 'java' },
-    { id: 2, name: 'javascript' },
-    { id: 3, name: 'python' }
+    { id: 1, name: 'javascript' },
+    { id: 2, name: 'python' },
+    { id: 3, name: 'java' }
 ];
 
 app.get('/courses', (req, res) => {
     res.json(courses);
+});
+
+app.post('/courses', (req, res) => {    
+    let course = {
+        id: courses.length + 1,
+        name: req.body.name
+    };
+    
+    courses.push(course);
+    res.json(courses);
+});
+
+app.put('/courses', (req, res) => {
+    const courseName = req.body.name;
+    let course = courses.find(c => c.name === courseName);
+
+    if (course) {
+        res.json(courses);
+    }
+    else {
+        let newCourse = {
+            id: courses.length + 1,
+            name: courseName
+        };
+
+        courses.push(newCourse);
+        res.json(courses);
+    }
 });
 
 app.listen(port, () => {
