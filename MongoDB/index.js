20258d4c64602d9c43ce4a729fb46e3ec791f327
Krupakar Reddy <137398727+Krupakar-Reddy-S@users.noreply.github.com>
2024-07-05 10:54:24 +0530

    added simple Product Schema

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -3,6 +3,7 @@ const express = require('express');
 const mongoose = require('mongoose');
 
 const app = express();
+const port = 8086;
 
 mongoose
     .connect(
@@ -15,7 +16,25 @@ mongoose
         console.log("Failed: ", err);
     });
 
-const port = 8086;
+// Product Schema
+const productSchema = new mongoose.Schema({
+    product_name : {
+        type : String,
+        required : true
+    },
+    product_price : {
+        type : String,
+        required : true
+    },
+    isInStock : {
+        type : Boolean,
+        required : true
+    },
+    category : {
+        type : String,
+        required : true
+    }
+});
 
 app.listen(port, () => {
     console.log(`Server is running on http://localhost:${port}`);
