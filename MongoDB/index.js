4c673d651ab9fee48d181d5ab96d8bdbb04a4359
Krupakar Reddy <137398727+Krupakar-Reddy-S@users.noreply.github.com>
2024-07-05 11:16:12 +0530

    create and read product data

--- MongoDB/index.js
+++ MongoDB/index.js
@@ -5,6 +5,8 @@ const mongoose = require('mongoose');
 const app = express();
 const port = 8086;
 
+app.use(express.json());
+
 mongoose
     .connect(
         process.env.MONGO_URI
@@ -34,6 +36,32 @@ const productSchema = new mongoose.Schema({
         type : String,
         required : true
     }
+});
+
+// Product Model
+const productModel = mongoose.model('Product', productSchema);
+
+// Create a new product
+app.post('/api/products', async (req, res) => {
+    const body = req.body;
+
+    const product = productModel.create({
+        product_name : body.product_name,
+        product_price : body.product_price,
+        isInStock : body.isInStock,
+        category : body.category
+    })
+
+    console.log(product);
+
+    return res.status(201).json({message : 'Product Created'});
+});
+
+// get products
+app.get('/api/products', async (req, res) => {
+    const products = await productModel.find({});
+
+    return res.status(200).json(products);
 });
 
 app.listen(port, () => {
